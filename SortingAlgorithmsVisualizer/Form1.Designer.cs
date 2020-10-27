129a9c9ad3806cc308efc85357e5cc685541e351
vladikshkwok <54217216+vladikshkwok@users.noreply.github.com>
2020-10-28 00:31:57 +0300

    Add project files.

---
+++ SortingAlgorithmsVisualizer/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace SortingAlgorithmsVisualizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BubbleSortButton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.массивToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ClearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.заполнитьРандомноToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.CoctailSortButton = new System.Windows.Forms.Button();
+            this.GnomeSortingButton = new System.Windows.Forms.Button();
+            this.MergeSortingButton = new System.Windows.Forms.Button();
+            this.SelectionSortingButton = new System.Windows.Forms.Button();
+            this.ArrayCapacitySetter = new System.Windows.Forms.TextBox();
+            this.OKButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 79);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 359);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BubbleSortButton
+            // 
+            this.BubbleSortButton.Location = new System.Drawing.Point(12, 31);
+            this.BubbleSortButton.Name = "BubbleSortButton";
+            this.BubbleSortButton.Size = new System.Drawing.Size(122, 42);
+            this.BubbleSortButton.TabIndex = 1;
+            this.BubbleSortButton.Text = "Сортировка пузырьком";
+            this.BubbleSortButton.UseVisualStyleBackColor = true;
+            this.BubbleSortButton.Click += new System.EventHandler(this.BubbleSortButton_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.массивToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // массивToolStripMenuItem
+            // 
+            this.массивToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ClearToolStripMenuItem,
+            this.заполнитьРандомноToolStripMenuItem});
+            this.массивToolStripMenuItem.Name = "массивToolStripMenuItem";
+            this.массивToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.массивToolStripMenuItem.Text = "Массив";
+            // 
+            // ClearToolStripMenuItem
+            // 
+            this.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem";
+            this.ClearToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.ClearToolStripMenuItem.Text = "Очистить";
+            this.ClearToolStripMenuItem.Click += new System.EventHandler(this.ClearToolStripMenuItem_Click);
+            // 
+            // заполнитьРандомноToolStripMenuItem
+            // 
+            this.заполнитьРандомноToolStripMenuItem.Name = "заполнитьРандомноToolStripMenuItem";
+            this.заполнитьРандомноToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.заполнитьРандомноToolStripMenuItem.Text = "Заполнить рандомно";
+            this.заполнитьРандомноToolStripMenuItem.Click += new System.EventHandler(this.FillRandToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // CoctailSortButton
+            // 
+            this.CoctailSortButton.Location = new System.Drawing.Point(140, 31);
+            this.CoctailSortButton.Name = "CoctailSortButton";
+            this.CoctailSortButton.Size = new System.Drawing.Size(122, 42);
+            this.CoctailSortButton.TabIndex = 3;
+            this.CoctailSortButton.Text = "Сортировка перемешиванием";
+            this.CoctailSortButton.UseVisualStyleBackColor = true;
+            this.CoctailSortButton.Click += new System.EventHandler(this.CoctailSortButton_Click);
+            // 
+            // GnomeSortingButton
+            // 
+            this.GnomeSortingButton.Location = new System.Drawing.Point(268, 31);
+            this.GnomeSortingButton.Name = "GnomeSortingButton";
+            this.GnomeSortingButton.Size = new System.Drawing.Size(122, 42);
+            this.GnomeSortingButton.TabIndex = 4;
+            this.GnomeSortingButton.Text = "Gnome сортировка (Stupid sort)";
+            this.GnomeSortingButton.UseVisualStyleBackColor = true;
+            this.GnomeSortingButton.Click += new System.EventHandler(this.GnomeSortingButton_Click);
+            // 
+            // MergeSortingButton
+            // 
+            this.MergeSortingButton.Location = new System.Drawing.Point(396, 31);
+            this.MergeSortingButton.Name = "MergeSortingButton";
+            this.MergeSortingButton.Size = new System.Drawing.Size(122, 42);
+            this.MergeSortingButton.TabIndex = 5;
+            this.MergeSortingButton.Text = "Сортировка слиянием";
+            this.MergeSortingButton.UseVisualStyleBackColor = true;
+            this.MergeSortingButton.Click += new System.EventHandler(this.MergeSortingButton_Click);
+            // 
+            // SelectionSortingButton
+            // 
+            this.SelectionSortingButton.Location = new System.Drawing.Point(524, 31);
+            this.SelectionSortingButton.Name = "SelectionSortingButton";
+            this.SelectionSortingButton.Size = new System.Drawing.Size(122, 42);
+            this.SelectionSortingButton.TabIndex = 6;
+            this.SelectionSortingButton.Text = "Сортировка выбором";
+            this.SelectionSortingButton.UseVisualStyleBackColor = true;
+            this.SelectionSortingButton.Click += new System.EventHandler(this.SelectionSortingButton_Click);
+            // 
+            // ArrayCapacitySetter
+            // 
+            this.ArrayCapacitySetter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ArrayCapacitySetter.Location = new System.Drawing.Point(590, 4);
+            this.ArrayCapacitySetter.Name = "ArrayCapacitySetter";
+            this.ArrayCapacitySetter.Size = new System.Drawing.Size(198, 20);
+            this.ArrayCapacitySetter.TabIndex = 7;
+            this.ArrayCapacitySetter.Tag = "";
+            this.ArrayCapacitySetter.Text = "Введите кол-во до 254 и нажмите ОК";
+            this.ArrayCapacitySetter.TextChanged += new System.EventHandler(this.ArrayCapacitySetter_TextChanged);
+            // 
+            // OKButton
+            // 
+            this.OKButton.Location = new System.Drawing.Point(705, 31);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(83, 42);
+            this.OKButton.TabIndex = 8;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            this.OKButton.Click += new System.EventHandler(this.OKButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.ArrayCapacitySetter);
+            this.Controls.Add(this.SelectionSortingButton);
+            this.Controls.Add(this.MergeSortingButton);
+            this.Controls.Add(this.GnomeSortingButton);
+            this.Controls.Add(this.CoctailSortButton);
+            this.Controls.Add(this.BubbleSortButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Визуализатор алгоритмов сортировки";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button BubbleSortButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem массивToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ClearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem заполнитьРандомноToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button CoctailSortButton;
+        private System.Windows.Forms.Button GnomeSortingButton;
+        private System.Windows.Forms.Button MergeSortingButton;
+        private System.Windows.Forms.Button SelectionSortingButton;
+        private System.Windows.Forms.TextBox ArrayCapacitySetter;
+        private System.Windows.Forms.Button OKButton;
+    }
+}
+
---
+++ SortingAlgorithmsVisualizer/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortingAlgorithmsVisualizer
+{
+    public partial class Form1 : Form
+    {
+        int ARRLENGTH = 250;
+        int[] unsortedArray;
+
+        Bitmap bitmap;
+        Graphics g;
+        Pen pen, penCurrent;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            GraphicsInit();
+            ArrayInit();
+        }
+        private void ArrayInit(int capacity=250)
+        {
+            OKButton.Enabled = false;
+            unsortedArray = new int[capacity];
+            FillArray(unsortedArray);
+        }
+        private void GraphicsInit()
+        {
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bitmap;
+            g = Graphics.FromImage(pictureBox1.Image);
+            pen = new Pen(Color.Black);
+            penCurrent = new Pen(Color.Red);
+            pen.Width = 2;
+            penCurrent.Width = 2;
+        }
+        private void DrawArray(int[] array, int current=-1)
+        {
+            g.Clear(Color.White);
+            for (int i = 0, j = 0; i < ARRLENGTH; i++, j += 2) {
+                if (i != current)
+                    g.DrawLine(pen, new Point(i + 15 + j, pictureBox1.Height), new Point(i + 15 + j, pictureBox1.Height - array[i]));
+                else
+                    g.DrawLine(penCurrent, new Point(i + 15 + j, pictureBox1.Height), new Point(i + 15 + j, pictureBox1.Height - array[i]));
+            }
+            pictureBox1.Refresh();
+        }
+
+        private void FillArray(int[] array)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < ARRLENGTH; i++)
+                array[i] = rand.Next(1, 200);
+            DrawArray(array);
+        }
+        static void Swap(int[] array, int i, int j)
+        {
+            int glass = array[i];
+            array[i] = array[j];
+            array[j] = glass;
+        }
+
+        private void printArray(int[] array)
+        {
+            for (int i = 0; i < ARRLENGTH; i++)
+                Console.Write($" {array[i]}");
+            Console.WriteLine();
+        }
+
+        #region ButtonsClickEvents
+
+        private void BubbleSortButton_Click(object sender, EventArgs e)
+        {
+            BubbleSorting(unsortedArray);
+        }
+        private void CoctailSortButton_Click(object sender, EventArgs e)
+        {
+            ShakerSorting(unsortedArray);
+        }
+        private void GnomeSortingButton_Click(object sender, EventArgs e)
+        {
+            GnomeSorting(unsortedArray);
+        }
+        private void MergeSortingButton_Click(object sender, EventArgs e)
+        {
+            MergeSort(unsortedArray, 0, ARRLENGTH-1);
+        }
+        private void SelectionSortingButton_Click(object sender, EventArgs e)
+        {
+            SelectionSorting(unsortedArray);
+        }
+        private void OKButton_Click(object sender, EventArgs e)
+        {
+            ARRLENGTH = Convert.ToInt32(ArrayCapacitySetter.Text);
+            ArrayInit(ARRLENGTH);
+            OKButton.Enabled = true;
+        }
+
+        private void ArrayCapacitySetter_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Convert.ToInt32(ArrayCapacitySetter.Text) <= 0 || Convert.ToInt32(ArrayCapacitySetter.Text) > 253)
+                    OKButton.Enabled = false;
+                else
+                {
+                    OKButton.Enabled = true;
+                }
+            }
+            catch (Exception er) { OKButton.Enabled = false; }
+        }
+
+
+        #endregion
+
+        #region StripMenuEvents
+        private void ClearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            pictureBox1.Refresh();
+        }
+
+        private void FillRandToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FillArray(unsortedArray);
+            DrawArray(unsortedArray);
+        }
+        #endregion
+
+        #region Sorting algorithms
+        private void BubbleSorting(int[] array)
+        {
+            for (int i = 0; i < ARRLENGTH; i++)
+            {
+                for (int j = i + 1; j < ARRLENGTH; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        DrawArray(array, j);
+                        Swap(array, i, j);
+                    }
+                }
+            }
+            DrawArray(array);
+        }
+
+        private void ShakerSorting(int[] array)
+        {
+            int left = 0,
+                right = ARRLENGTH - 1;
+                
+            while (left < right)
+            {
+                for (int i = left; i < right; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        Swap(array, i, i + 1);
+                        DrawArray(array, i);
+                    }
+                }
+                right--;
+
+                for (int i = right; i > left; i--)
+                {
+                    if (array[i - 1] > array[i])
+                    {
+                        Swap(array, i - 1, i);
+                        DrawArray(array, i);
+                    }
+                }
+                left++;
+            }
+            DrawArray(array);
+        }
+
+        private void GnomeSorting(int[] array)
+        {
+            int i = 1, j = 2;
+            while(i < ARRLENGTH)
+            {
+                if (array[i-1] < array[i])
+                {
+                    i = j;
+                    j++;
+                }
+                else
+                {
+                    Swap(array, i - 1, i);
+                    DrawArray(array, i);
+                    i--;
+                    if (i == 0)
+                    {
+                        i = j;
+                        j++;
+                    }
+                }
+            }
+            DrawArray(array);
+        }
+
+        private void Merge(int[] array, int left, int middle, int right)
+        {
+            int[] leftArray = new int[middle - left + 1];
+            int[] rightArray = new int[right - middle];
+
+            Array.Copy(array, left, leftArray, 0, middle - left + 1);
+            Array.Copy(array, middle + 1, rightArray, 0, right - middle);
+
+            int i = 0;
+            int j = 0;
+            for (int k = left; k < right + 1; k++)
+            {
+                if (i == leftArray.Length)
+                {
+                    array[k] = rightArray[j];
+                    j++;
+                }
+                else if (j == rightArray.Length)
+                {
+                    array[k] = leftArray[i];
+                    i++;
+                }
+                else if (leftArray[i] <= rightArray[j])
+                {
+                    array[k] = leftArray[i];
+                    i++;
+                }
+                else
+                {
+                    array[k] = rightArray[j];
+                    j++;
+                }
+                DrawArray(array, k);
+            }
+            DrawArray(array);
+        }
+
+
+
+        private void MergeSort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                
+                int middle = (left + right) / 2;
+                DrawArray(array);
+                MergeSort(array, left, middle);
+                MergeSort(array, middle + 1, right);
+                
+                Merge(array, left, middle, right);
+            }
+            
+        }
+        private void SelectionSorting(int[] array)
+        {
+            for (int i = 0; i < ARRLENGTH; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < ARRLENGTH; j++)
+                {
+                    if (array[j] < array[min])
+                        min = j;
+                    DrawArray(array, j);
+                }
+                Swap(array, i, min);
+
+                DrawArray(array, min);
+            }
+            DrawArray(array);
+        }
+
+        #endregion
+
+
+    }
+}
